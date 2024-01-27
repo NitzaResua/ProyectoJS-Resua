54928af9a3c91b8c52278ce1e56b2d34e0dcf377
Nitza Resua <nitza.resua@gmail.com>
2024-01-26 21:41:52 -0300

    modificaciones de mayor de edad

--- js/index.js
+++ js/index.js
@@ -1,48 +1,47 @@
 let nombreDeUsuario = prompt("Ingresa nombre de usuario");
 let contrasena = prompt("ingresa tu contraseña");
 
-if (nombreDeUsuario === "Alumno" && (contrasena === "123456" || contrasena === "000000")){
-    console.log("Permitir acceso");
+if (nombreDeUsuario === "Nombre" && (contrasena === "123456" || contrasena === "000000")){
+    console.log("Permitir acceso. Hola, bienvenido.");
 }else{
-    console.log("Acceso denegado");
+    console.log("Olvidaste la contraseña o no estas registrado");
 }
 
 
 function comprobarEdad (){
     const EDAD = parseInt(prompt("Ingresar edad"));
     if(EDAD >= 18){
-        return"Es mayor de edad."
-    }else{
+        const COTIZACION_DOLAR = 850;
+        const COTIZACION_EUROS = 950
+        const PESO_DOLAR = (pesos) => pesos / COTIZACION_DOLAR;
+        const DOLAR_PESO = (dolar) => dolar * COTIZACION_DOLAR;
+        const PESO_EURO = (pesos) => pesos / COTIZACION_EUROS;
+        const EURO_PESO = (euro) => euro * COTIZACION_EUROS;
+        let seleccion = prompt("SELECCIONAR COTIZACION \n 1 - PESOS A DOLAR \n 2 -  DOLARES A PESOS \n 3 - PESOS A EURO \n 4 -  EUROS A PESOS");
+        let valor = prompt("VALOR");
+            switch (seleccion) {
+            case "1":
+                console.log("La cotizacion es: "+ valor + " peso argentino, es " + PESO_DOLAR(valor)+ " dolares.");
+            break;
+            case "2":
+                console.log("La cotizacion es: "+ valor + " dolar, es " + DOLAR_PESO(valor)+ "  pesos argentinos.");
+            break;
+            case "3":
+                console.log("La cotizacion es: "+ valor + " peso argentino, es " + PESO_EURO(valor)+ " euros.");
+            break;
+            case "4":
+                console.log("La cotizacion es: "+ valor + " euro, es " + EURO_PESO(valor)+ "  pesos argentinos.");
+            break;
+            default:
+                console.log("No se pudo realizar la cotizacion");
+            }
+        return"Gracias por utilizar el conversor de divisas."
+        }else{
         return "No es mayor de edad."
     }
 }
 const resultadoEdad = comprobarEdad();
 
 console.log(resultadoEdad);
-
-const COTIZACION_DOLAR = 850;
-const COTIZACION_EUROS= 950
-const cotizarDolar = (pesos) => pesos / COTIZACION_DOLAR;
-const cotizarPesosDolar = (dolar) => dolar * COTIZACION_DOLAR;
-const cotizarEuro = (pesos) => pesos / COTIZACION_EUROS;
-const cotizarPesosEuro = (euro) => euro * COTIZACION_EUROS;
-let seleccion = prompt("SELECCIONAR COTIZACION \n 1 - DOLARES A PESOS \n 2 -  PESOS A DOLAR \n 3 - EUROS A PESOS \n 4 -  PESOS A EURO");
-let valor = prompt("VALOR");
-switch (seleccion) {
-    case "1":
-        alert(cotizarPesosDolar(valor));
-        break;
-    case "2":
-        alert(cotizarDolar(valor));
-        break;
-        case "3":
-        alert(cotizarPesosEuro(valor));
-        break;
-        case "4":
-        alert(cotizarEuro(valor));
-        break;
-    default:
-        break;
-}
 
 
